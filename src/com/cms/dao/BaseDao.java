1eea9c2135013dab4cd0347e9f340e60f92f88d2
andy <1015246310@qq.com>
2014-02-27 21:34:54 +0800

    add base dao

---
+++ src/com/cms/dao/BaseDao.java
@@ -0,0 +1,7 @@
+package com.cms.dao;
+
+/**
+ * Created by Administrator on 14-2-27.
+ */
+public interface BaseDao {
+}
---
+++ src/com/cms/dao/impl/BaseDaoImpl.java
@@ -0,0 +1,9 @@
+package com.cms.dao.impl;
+
+import com.cms.dao.BaseDao;
+
+/**
+ * Created by Administrator on 14-2-27.
+ */
+public class BaseDaoImpl implements BaseDao {
+}
