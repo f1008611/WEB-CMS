d2a7404c9aff9df6c7980048cdb7b1cb9d079836
f1008611 <andyadny1988@gmail.com>
2014-03-04 10:26:27 +0800

    f

--- src/main/java/com/cms/action/CmsUserAction.java
+++ src/main/java/com/cms/action/CmsUserAction.java
@@ -2,9 +2,12 @@ package com.cms.action;
 
 import com.cms.pojo.CmsUser;
 import com.cms.service.CmsUserService;
+import com.opensymphony.xwork2.ActionSupport;
 import com.opensymphony.xwork2.ModelDriven;
 import org.apache.struts2.convention.annotation.Action;
 import org.apache.struts2.convention.annotation.Namespace;
+import org.apache.struts2.convention.annotation.Result;
+import org.apache.struts2.convention.annotation.Results;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
@@ -14,8 +17,9 @@ import org.springframework.stereotype.Controller;
  */
 @Namespace("/admin")
 @Action(value = "cmsUserAction")
+@Results({@Result(name="aa",location="/WEB-INF/admin/index.jsp")})
 @Controller
-public class CmsUserAction {
+public class CmsUserAction{
 
 
     private String userName;
@@ -26,7 +30,7 @@ public class CmsUserAction {
     public String login(){
         System.out.println("userName="+userName+"======password="+password);
         cmsUserService.login(userName,password);
-        return null;
+        return "aa";
     }
 
 
