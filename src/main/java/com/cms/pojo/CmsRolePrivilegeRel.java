f5b07fed05fade7d05c817ddba62300dc3341ca3
f1008611 <andyadny1988@gmail.com>
2014-03-19 17:24:19 +0800

    commit

---
+++ src/main/java/com/cms/pojo/CmsRolePrivilegeRel.java
@@ -0,0 +1,45 @@
+package com.cms.pojo;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by Administrator on 14-3-19.
+ */
+@Entity
+@Table(name="cms_role_privilege_rel")
+public class CmsRolePrivilegeRel implements Serializable {
+    private static final Long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(name="role_id")
+    private Long roleId;
+    @Column(name = "privilege_id")
+    private Long privilegeId;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public Long getPrivilegeId() {
+        return privilegeId;
+    }
+
+    public void setPrivilegeId(Long privilegeId) {
+        this.privilegeId = privilegeId;
+    }
+}
