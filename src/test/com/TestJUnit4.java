afce7fb5b454c8ae133ab1c737a967de40996ad5
f1008611 <andyadny1988@gmail.com>
2014-03-20 10:49:04 +0800

    commit111

---
+++ src/test/com/TestJUnit4.java
@@ -0,0 +1,32 @@
+package com;
+
+
+import com.cms.pojo.CmsUser;
+import com.cms.service.CmsUserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Administrator on 14-3-19.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath*:spring/spring.xml","classpath*:hibernate/hibernate.cfg.xml","classpath*:struts/struts.xml"})
+public class TestJUnit4 {
+
+    @Autowired
+    private CmsUserService cmsUserService;
+
+    @Test
+    public  void testUser(){
+        CmsUser cmsUser= cmsUserService.findByUserName("a");
+        if(cmsUser!=null){
+            System.out.println("id="+cmsUser.getId()+",userName="+cmsUser.getUserName()+",createTime="+cmsUser.getCreateTime());
+        }else {
+            System.out.println("no user to find!");
+        }
+    }
+
+}
