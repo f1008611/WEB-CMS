792ba2593cfcb279e686484de9857866fbd14def
f1008611 <andyadny1988@gmail.com>
2014-04-04 16:54:34 +0800

    commit

--- src/test/com/TestIterator.java
+++ src/test/com/TestIterator.java
@@ -1,6 +1,7 @@
 package com;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,6 +11,7 @@ import java.util.List;
 public class TestIterator{
     public static void main(String[] args) {
         List list=new ArrayList();
+
         list.add("dd");
         list.add("fsd");
         list.add("dfsa");
@@ -21,6 +23,22 @@ public class TestIterator{
         while (item.hasNext()){
             System.out.println((String)item.next());
         }
+        System.out.println(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>");
+        for(Iterator it=list.iterator();it.hasNext();){
+            System.out.println((String)it.next());
+        }
+
+
+        HashSet set=new HashSet();
+        set.add("fff");
+        set.add("sd");
+        set.add("fds");
+        set.add("eew");
+        set.add("ggg");
+
+       /* for(int i=0;i<set.size();i++){
+            System.out.println(set.get());
+        }*/
 
 
     }
